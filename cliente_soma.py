a355c88914963562b18a92f31b1d9c3f5cff0b3e
Arthur <arthurjaf@gmail.com>
2026-04-29 22:08:35 -0300

    Soma de vetor de 10k em diferentes maquinas

---
+++ cliente_soma.py
@@ -0,0 +1,21 @@
+"""Aqui vamos testar o cliente passando o tamanho do vetor a ser somado"""
+
+import time
+import rpyc
+import sys
+
+if len(sys.argv) < 2:
+    exit("Usage {} SERVER".format(sys.argv[0]))
+
+server = sys.argv[1]
+conn = rpyc.connect(server, 18861)
+
+n = int(input("Digite o tamanho do vetor a ser somado: "))
+arr = list(range(n))
+
+start = time.time()
+resultado = conn.root.sum_array(arr)
+end = time.time()
+
+print("Tempo de execução no cliente: {}".format(end - start))
+print("A soma do vetor é: {}".format(resultado))
---
+++ server_soma.py
@@ -0,0 +1,25 @@
+"""Aqui vamos testar o servidor fazendo a soma de vetores passados pelo cliente"""
+
+import time
+import rpyc
+
+class MyService(rpyc.Service):
+
+    def on_connect(self, conn):
+        pass
+
+    def on_disconnect(self, conn):
+        pass
+
+    def exposed_sum_array(self, arr):
+        start = time.time()
+        result = sum(arr)
+        end = time.time()
+        print("Tempo de execução: {}".format(end - start))
+        return result
+
+
+if __name__ == "__main__":
+    from rpyc.utils.server import ThreadedServer
+    t = ThreadedServer(MyService, port=18861)
+    t.start()
